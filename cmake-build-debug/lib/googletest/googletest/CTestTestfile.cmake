3600bfd1ffb7afdef1b9f519398e5b6fb3c4031b
Harshit Agrawal <harshitagrawal2004@gmail.com>
2025-09-30 18:43:08 +0530

    basic imporvement and enviroment setup for rl model
    included reset function. getter function to get the object varialbes. and enviroment setup for the rl model which include converting the board into 1d int vector, converting the legal move map into int map, a vector of all the legal moves converted into int and step function which will make the move get updated board state, reward and is the game ended or not

---
+++ cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/apple/Documents/programing/porjects/othello/lib/googletest/googletest
+# Build directory: /Users/apple/Documents/programing/porjects/othello/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
